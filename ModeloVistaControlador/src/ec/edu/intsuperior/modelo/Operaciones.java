bc2232bbde5628485028eeeb31ee2cd0c4fb6941
wssolis <sys.solis@gmail.com>
2020-07-20 22:14:16 -0500

    Version 2

---
+++ ModeloVistaControlador/src/ec/edu/intsuperior/modelo/Operaciones.java
@@ -0,0 +1,17 @@
+
+package ec.edu.intsuperior.modelo;
+
+public class Operaciones {
+    public int suma(int n1, int n2){
+        return n1+n2;
+    }
+     public int resta(int n1, int n2){
+        return n1-n2;
+    }
+     public int producto(int n1, int n2){
+        return n1*n2;
+    }
+      public int division(int n1, int n2){
+        return n1/n2;
+    }
+}
